f211c16f1ceb82bf0ed7e75a9ae69fbe13f1c3ee
akashm01github <akashmukhopadhayay@gmail.com>
2025-04-01 22:25:01 +0530

    Sorting Alogorithm

---
+++ Day-5/app.js
@@ -0,0 +1,56 @@
+// Sorting Algorithm
+//********************************************************
+// Bubble Sort
+
+// let arr = [50, 7, 8, 3, 2, 10];
+// let n = arr.length;
+// for (let i = 0; i < arr.length - 1; i++) {
+//   for (let j = 0; j < n - i + 1; j++) {
+//     if (arr[j] > arr[j + 1]) {
+//       let temp = arr[j];
+//       arr[j] = arr[j + 1];
+//       arr[j + 1] = temp;
+//     }
+//   }
+// }
+
+// console.log(arr);
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+//Selection  sort
+// let arr = [50, 7, 8, 3, 2, 10];
+
+// for (let i = 0; i < arr.length - 1; i++) {
+//   let small = i;
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[small] > arr[j]) {
+//       small = j;
+//     }
+//   }
+//   if (i !== small) {
+//     let temp = arr[i];
+//     arr[i] = arr[small];
+//     arr[small] = temp;
+//   }
+// }
+
+// console.log(arr);
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+//Insertion sort
+
+let arr = [50, 7, 8, 3, 2, 10];
+
+for (let i = 0; i < arr.length - 1; i++) {
+  let key = arr[i];
+  let j = i - 1;
+  while (j >= 0 && arr[j] > key) {
+    arr[j + 1] = arr[j];
+    j--;
+  }
+  arr[j + 1] = key;
+}
+
+console.log(arr);

3147fafa86426e371adb1a4308de48d51acafd0c
akashm01github <akashmukhopadhayay@gmail.com>
2025-09-23 19:07:48 +0530

    Day-21

--- Day-20/script.js
+++ Day-20/script.js
@@ -63,12 +63,74 @@
 
 // spread and rest 
 
-function addNumber(a,b, ...others){
-    console.log(a)
-    console.log(b)
+// function addNumber(a,b, ...others){
+//     console.log(a)
+//     console.log(b)
     // console.log(c)
-    console.log(others)
-}
+//     console.log(others)
+// }
 
 
-addNumber(10, 20, 30, 40, 50);
+// addNumber(10, 20, 30, 40, 50);
+
+
+//########################################################################
+
+//! 🔹 forEach Practice
+
+// let nums = [2, 4, 6, 8];
+
+// nums.forEach(function(elem){
+//     console.log(`Number is ${elem}`);
+// })
+
+
+// let names  =  ["Akash", "Aparna", "Papan", "Jharnna"];
+
+
+// names.forEach(function(personName){
+//     console.log(`Hello ${personName}`)
+// })
+
+
+//! map Practice
+
+// let nums = [1, 2, 3, 4];
+
+// let updatedNums = nums.map(function(number){
+//     return number *2;
+// })
+
+// console.log(updatedNums)
+
+
+// let fruits = ["apple", "banana", "mango"];
+
+// let lengthofFruits = fruits.map(function(fruit){
+//         return fruit.length;
+// })
+
+// console.log(lengthofFruits)
+
+
+//! filter Practice
+
+
+// let nums = [10, 15, 20, 25, 30];
+
+// let evenNumbers = nums.filter(function(num){
+//     return num % 2 == 0;
+// })
+
+
+// console.log(evenNumbers);
+
+
+let animals = ["cat", "elephant", "dog", "tiger"];
+
+let updatedAnimales = animals.filter(function(animal){
+     return animal.length > 3 ;
+})
+
+
+console.log(updatedAnimales);
---
+++ Day-21/script.js
@@ -0,0 +1,30 @@
+//! Function Approach (Old Method)
+
+// function Toofie(myFlavour,myPrice,toffieExpiry){
+//     this.flavour = myFlavour;
+//     this.price = myPrice;
+//     this.expiry = toffieExpiry;
+// }
+
+
+// let t1 = new Toofie("Vanila",10,"23-05-2025");
+// let t2 = new Toofie("Chocolate",30,"24-05-2025");
+
+
+//!  Class Approach (Modern Way) 
+
+
+class Toffie {
+    constructor(flavour,color,taste,texture){
+        this.flavour = flavour;
+        this.color = color;
+        this.taste = taste;
+        this.texture = texture;
+    }
+}
+
+let t1 = new Toffie("Vanila","Brown","Sweet","Hard");
+let t2 = new Toffie("Chocolate","Yellow","Tasty","Soft");
+
+console.log(t1);
+console.log(t2);
